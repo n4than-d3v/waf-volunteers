e649b94f447802f4b0598faf0bb7b85ffce1b13a
Nathan Gradidge <nathan.gradidge@triad.co.uk>
2025-11-25 10:27:50 +0000

    fix refresh loop

--- portal/src/app/app.component.ts
+++ portal/src/app/app.component.ts
@@ -1,12 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  ActivatedRoute,
-  Router,
-  RouterLink,
-  RouterOutlet,
-} from '@angular/router';
+import { Component } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { TokenProvider } from './shared/token.provider';
-import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -14,22 +8,12 @@ import { SwUpdate } from '@angular/service-worker';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   year: number = new Date().getFullYear();
 
-  constructor(
-    private tokenProvider: TokenProvider,
-    public router: Router,
-    private swUpdate: SwUpdate
-  ) {}
+  constructor(private tokenProvider: TokenProvider, public router: Router) {}
 
   signOut() {
     this.tokenProvider.setToken('');
-  }
-
-  ngOnInit() {
-    this.swUpdate.versionUpdates.subscribe(() => {
-      this.swUpdate.activateUpdate().then(() => document.location.reload());
-    });
   }
 }

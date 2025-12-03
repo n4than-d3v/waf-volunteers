356e9c719d51505572681cc76be148f01d351794
Nathan Gradidge <nathan.gradidge@triad.co.uk>
2025-12-03 18:45:46 +0000

    requirements ordering

--- api/Handlers/Rota/Misc/Requirements/GetRequirementsHandler.cs
+++ api/Handlers/Rota/Misc/Requirements/GetRequirementsHandler.cs
@@ -21,6 +21,9 @@ public class GetRequirementsHandler : IRequestHandler<GetRequirements, IResult>
     public async Task<IResult> Handle(GetRequirements request, CancellationToken cancellationToken)
     {
         var requirements = await _repository.GetAll<Requirement>(x => true, tracking: false, action: x => x.Include(y => y.Time).Include(y => y.Job));
-        return Results.Ok(requirements.OrderBy(x => x.Id));
+        return Results.Ok(requirements
+            .OrderBy(x => x.Day == DayOfWeek.Sunday ? 7 : (int)x.Day)
+            .ThenBy(x => x.Time.Id)
+            .ThenBy(x => x.Job.Id));
     }
 }

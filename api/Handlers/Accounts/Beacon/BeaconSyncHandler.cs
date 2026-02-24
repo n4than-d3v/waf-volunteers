8dbd8b157385b1f8dfcb577d0677f2ea8f0911f7
Nathan Gradidge <nathan.gradidge@triad.co.uk>
2026-02-24 09:26:07 +0000

    figure out what's up with beacon

--- api/Handlers/Accounts/Beacon/BeaconSyncHandler.cs
+++ api/Handlers/Accounts/Beacon/BeaconSyncHandler.cs
@@ -30,208 +30,215 @@ public class BeaconSyncHandler : IRequestHandler<BeaconSync, IResult>
 
     public async Task<IResult> Handle(BeaconSync request, CancellationToken cancellationToken)
     {
-        var activeVolunteers = await _beaconService.GetActiveVolunteersAsync();
-        var formerVolunteers = await _beaconService.GetFormerVolunteersAsync();
-
-        var accounts = await _repository.GetAll<Account>(x => true);
-        var times = await _repository.GetAll<TimeRange>(x => true);
-        var jobs = await _repository.GetAll<Job>(x => true);
-
-        var newlyCreatedAccountUsernames = new List<string>();
-
-        // Ensure all former volunteers are marked as inactive
-        foreach (var formerVolunteer in formerVolunteers.results)
+        try
         {
-            var account = accounts.FirstOrDefault(x => x.BeaconId == formerVolunteer.entity.id);
-            account?.UpdateStatus(AccountStatus.Inactive);
-        }
+            var activeVolunteers = await _beaconService.GetActiveVolunteersAsync();
+            var formerVolunteers = await _beaconService.GetFormerVolunteersAsync();
 
-        // Ensure all active volunteers have an account created
-        foreach (var activeVolunteer in activeVolunteers.results)
-        {
-            var account = accounts.FirstOrDefault(x => x.BeaconId == activeVolunteer.entity.id);
-            if (account != null) continue;
+            var accounts = await _repository.GetAll<Account>(x => true);
+            var times = await _repository.GetAll<TimeRange>(x => true);
+            var jobs = await _repository.GetAll<Job>(x => true);
 
-            var primaryEmail = activeVolunteer.entity.emails.FirstOrDefault(x => x.is_primary);
-            if (primaryEmail == null) continue;
+            var newlyCreatedAccountUsernames = new List<string>();
 
-            account = accounts.FirstOrDefault(x => _encryptionService.Decrypt(x.Email, x.Salt) == primaryEmail.email);
-            if (account != null) continue;
-
-            int lettersOfFirstName = 1;
-
-            string username = (activeVolunteer.entity.name.first[0] + activeVolunteer.entity.name.last)
-                .ToLower().Replace("-", "").Replace("'", "").Replace(" ", "");
-
-            int tries = 1;
-            while (true)
+            // Ensure all former volunteers are marked as inactive
+            foreach (var formerVolunteer in formerVolunteers.results)
             {
-                var existingUser = accounts.FirstOrDefault(x => x.Username == username);
-                if (existingUser == null && !newlyCreatedAccountUsernames.Contains(username)) break;
-                username = (activeVolunteer.entity.name.first[..lettersOfFirstName] + activeVolunteer.entity.name.last)
-                    .ToLower().Replace("-", "").Replace("'", "").Replace(" ", "");
-                if (lettersOfFirstName >= activeVolunteer.entity.name.first.Length)
-                {
-                    tries++;
-                    username = (activeVolunteer.entity.name.first[0] + activeVolunteer.entity.name.last + tries)
-                        .ToLower().Replace("-", "").Replace("'", "").Replace(" ", "");
-                }
-                else
-                {
-                    lettersOfFirstName++;
-                }
+                var account = accounts.FirstOrDefault(x => x.BeaconId == formerVolunteer.entity.id);
+                account?.UpdateStatus(AccountStatus.Inactive);
             }
 
-            newlyCreatedAccountUsernames.Add(username);
-
-            var salt = _encryptionService.GenerateSalt();
-
-            account = new Account(
-                username,
-                password: "-",
-                AccountStatus.Active,
-                AccountRoles.None,
-                lastLoggedIn: null,
-                userAgent: null,
-                _encryptionService.Encrypt(string.Empty, salt),
-                _encryptionService.Encrypt(string.Empty, salt),
-                _encryptionService.Encrypt(string.Empty, salt),
-                activeVolunteer.entity.id,
-                _encryptionService.Encrypt(string.Empty, salt),
-                [],
-                _encryptionService.Encrypt(string.Empty, salt),
-                salt
-            );
-
-            _repository.Create(account);
-        }
-
-        await _repository.SaveChangesAsync();
-        accounts = await _repository.GetAll<Account>(x => true);
-        var regularShifts = await _repository.GetAll<RegularShift>(x => true,
-            action: x => x.Include(y => y.Account).Include(y => y.Time).Include(y => y.Job));
-
-        foreach (var activeVolunteer in activeVolunteers.results)
-        {
-            var account = accounts.FirstOrDefault(x => x.BeaconId == activeVolunteer.entity.id);
-
-            var primaryEmail = activeVolunteer.entity.emails.FirstOrDefault(x => x.is_primary);
-            if (primaryEmail == null) continue;
-
-            if (account == null)
+            // Ensure all active volunteers have an account created
+            foreach (var activeVolunteer in activeVolunteers.results)
             {
+                var account = accounts.FirstOrDefault(x => x.BeaconId == activeVolunteer.entity.id);
+                if (account != null) continue;
+
+                var primaryEmail = activeVolunteer.entity.emails.FirstOrDefault(x => x.is_primary);
+                if (primaryEmail == null) continue;
+
                 account = accounts.FirstOrDefault(x => _encryptionService.Decrypt(x.Email, x.Salt) == primaryEmail.email);
-                if (account == null) continue;
+                if (account != null) continue;
 
-                account.UpdateBeaconId(activeVolunteer.entity.id);
-            }
+                int lettersOfFirstName = 1;
 
-            // Ensure all active volunteers have the latest updateable info
-            var firstName = _encryptionService.Encrypt(activeVolunteer.entity.name.first, account.Salt);
-            if (!string.IsNullOrWhiteSpace(activeVolunteer.entity.c_preferred_name))
-            {
-                firstName = _encryptionService.Encrypt(activeVolunteer.entity.c_preferred_name, account.Salt);
-            }
-            var lastName = _encryptionService.Encrypt(activeVolunteer.entity.name.last, account.Salt);
-            var email = _encryptionService.Encrypt(primaryEmail.email, account.Salt);
+                string username = (activeVolunteer.entity.name.first[0] + activeVolunteer.entity.name.last)
+                    .ToLower().Replace("-", "").Replace("'", "").Replace(" ", "");
 
-            var json = JsonConvert.SerializeObject(activeVolunteer.entity);
-            var beaconInfo = _encryptionService.Encrypt(json, account.Salt);
-            account.UpdateStatus(AccountStatus.Active);
-            account.UpdatePersonalDetails(firstName, lastName, email, beaconInfo, account.Cars);
-
-            AccountRoles roles = 0;
-
-            activeVolunteer.entity.volunteer_roles.ForEach(role =>
-            {
-                roles |= (AccountRoles)Enum.Parse(typeof(AccountRoles), $"BEACON_{role.Replace(" ", "_").ToUpper()}");
-            });
-
-            var job = jobs.OrderByDescending(x => x.Id).FirstOrDefault(x => roles.HasFlag(x.BeaconAssociatedRole));
-
-            account.UpdateRoles(roles);
-
-            if (job == null) continue;
-
-            // Do not manage regular shifts for existing accounts
-            if (!newlyCreatedAccountUsernames.Contains(account.Username)) continue;
-
-            var availability = activeVolunteer.entity.volunteer_availability;
-            List<RegularShift> checkedRegularShifts = [];
-            foreach (var shift in availability)
-            {
-                var split = shift.Split(' ');
-                string dayString, weekString = null, timeString;
-                if (split.Length == 2)
+                int tries = 1;
+                while (true)
                 {
-                    dayString = split[0];
-                    timeString = split[1];
-
-                }
-                else if (split.Length == 3)
-                {
-                    dayString = split[0];
-                    weekString = split[1];
-                    timeString = split[2];
-                }
-                else
-                {
-                    continue;
-                }
-
-                DayOfWeek day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), dayString);
-                TimeRange time = times.FirstOrDefault(x => x.BeaconName == timeString);
-                if (time == null) continue;
-
-                int? week = null;
-                if (weekString != null)
-                {
-                    if (int.TryParse(weekString, out int tempWeek))
+                    var existingUser = accounts.FirstOrDefault(x => x.Username == username);
+                    if (existingUser == null && !newlyCreatedAccountUsernames.Contains(username)) break;
+                    username = (activeVolunteer.entity.name.first[..lettersOfFirstName] + activeVolunteer.entity.name.last)
+                        .ToLower().Replace("-", "").Replace("'", "").Replace(" ", "");
+                    if (lettersOfFirstName >= activeVolunteer.entity.name.first.Length)
                     {
-                        week = tempWeek;
+                        tries++;
+                        username = (activeVolunteer.entity.name.first[0] + activeVolunteer.entity.name.last + tries)
+                            .ToLower().Replace("-", "").Replace("'", "").Replace(" ", "");
+                    }
+                    else
+                    {
+                        lettersOfFirstName++;
+                    }
+                }
+
+                newlyCreatedAccountUsernames.Add(username);
+
+                var salt = _encryptionService.GenerateSalt();
+
+                account = new Account(
+                    username,
+                    password: "-",
+                    AccountStatus.Active,
+                    AccountRoles.None,
+                    lastLoggedIn: null,
+                    userAgent: null,
+                    _encryptionService.Encrypt(string.Empty, salt),
+                    _encryptionService.Encrypt(string.Empty, salt),
+                    _encryptionService.Encrypt(string.Empty, salt),
+                    activeVolunteer.entity.id,
+                    _encryptionService.Encrypt(string.Empty, salt),
+                    [],
+                    _encryptionService.Encrypt(string.Empty, salt),
+                    salt
+                );
+
+                _repository.Create(account);
+            }
+
+            await _repository.SaveChangesAsync();
+            accounts = await _repository.GetAll<Account>(x => true);
+            var regularShifts = await _repository.GetAll<RegularShift>(x => true,
+                action: x => x.Include(y => y.Account).Include(y => y.Time).Include(y => y.Job));
+
+            foreach (var activeVolunteer in activeVolunteers.results)
+            {
+                var account = accounts.FirstOrDefault(x => x.BeaconId == activeVolunteer.entity.id);
+
+                var primaryEmail = activeVolunteer.entity.emails.FirstOrDefault(x => x.is_primary);
+                if (primaryEmail == null) continue;
+
+                if (account == null)
+                {
+                    account = accounts.FirstOrDefault(x => _encryptionService.Decrypt(x.Email, x.Salt) == primaryEmail.email);
+                    if (account == null) continue;
+
+                    account.UpdateBeaconId(activeVolunteer.entity.id);
+                }
+
+                // Ensure all active volunteers have the latest updateable info
+                var firstName = _encryptionService.Encrypt(activeVolunteer.entity.name.first, account.Salt);
+                if (!string.IsNullOrWhiteSpace(activeVolunteer.entity.c_preferred_name))
+                {
+                    firstName = _encryptionService.Encrypt(activeVolunteer.entity.c_preferred_name, account.Salt);
+                }
+                var lastName = _encryptionService.Encrypt(activeVolunteer.entity.name.last, account.Salt);
+                var email = _encryptionService.Encrypt(primaryEmail.email, account.Salt);
+
+                var json = JsonConvert.SerializeObject(activeVolunteer.entity);
+                var beaconInfo = _encryptionService.Encrypt(json, account.Salt);
+                account.UpdateStatus(AccountStatus.Active);
+                account.UpdatePersonalDetails(firstName, lastName, email, beaconInfo, account.Cars);
+
+                AccountRoles roles = 0;
+
+                activeVolunteer.entity.volunteer_roles.ForEach(role =>
+                {
+                    roles |= (AccountRoles)Enum.Parse(typeof(AccountRoles), $"BEACON_{role.Replace(" ", "_").ToUpper()}");
+                });
+
+                var job = jobs.OrderByDescending(x => x.Id).FirstOrDefault(x => roles.HasFlag(x.BeaconAssociatedRole));
+
+                account.UpdateRoles(roles);
+
+                if (job == null) continue;
+
+                // Do not manage regular shifts for existing accounts
+                if (!newlyCreatedAccountUsernames.Contains(account.Username)) continue;
+
+                var availability = activeVolunteer.entity.volunteer_availability;
+                List<RegularShift> checkedRegularShifts = [];
+                foreach (var shift in availability)
+                {
+                    var split = shift.Split(' ');
+                    string dayString, weekString = null, timeString;
+                    if (split.Length == 2)
+                    {
+                        dayString = split[0];
+                        timeString = split[1];
+
+                    }
+                    else if (split.Length == 3)
+                    {
+                        dayString = split[0];
+                        weekString = split[1];
+                        timeString = split[2];
                     }
                     else
                     {
                         continue;
                     }
-                }
 
-                var existingRegularShift = regularShifts.FirstOrDefault(x => x.Account.Id == account.Id && x.Day == day && x.Time.Id == time.Id && x.Job.Id == job.Id);
-                if (existingRegularShift == null)
-                {
-                    existingRegularShift = new RegularShift
+                    DayOfWeek day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), dayString);
+                    TimeRange time = times.FirstOrDefault(x => x.BeaconName == timeString);
+                    if (time == null) continue;
+
+                    int? week = null;
+                    if (weekString != null)
                     {
-                        Account = account,
-                        Day = day,
-                        Week = week,
-                        Job = job,
-                        Time = time
-                    };
-                    _repository.Create(existingRegularShift);
+                        if (int.TryParse(weekString, out int tempWeek))
+                        {
+                            week = tempWeek;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+
+                    var existingRegularShift = regularShifts.FirstOrDefault(x => x.Account.Id == account.Id && x.Day == day && x.Time.Id == time.Id && x.Job.Id == job.Id);
+                    if (existingRegularShift == null)
+                    {
+                        existingRegularShift = new RegularShift
+                        {
+                            Account = account,
+                            Day = day,
+                            Week = week,
+                            Job = job,
+                            Time = time
+                        };
+                        _repository.Create(existingRegularShift);
+                    }
+                    checkedRegularShifts.Add(existingRegularShift);
                 }
-                checkedRegularShifts.Add(existingRegularShift);
+
+                var deleteRegularShifts = regularShifts.Where(x => x.Account.Id == account.Id && !checkedRegularShifts.Contains(x));
+                foreach (var deleteRegularShift in deleteRegularShifts)
+                {
+                    _repository.Delete(deleteRegularShift);
+                }
             }
 
-            var deleteRegularShifts = regularShifts.Where(x => x.Account.Id == account.Id && !checkedRegularShifts.Contains(x));
-            foreach (var deleteRegularShift in deleteRegularShifts)
+            foreach (var newlyCreatedAccountUsername in newlyCreatedAccountUsernames)
             {
-                _repository.Delete(deleteRegularShift);
+                /*
+                await _mediator.Send(new RequestPasswordReset
+                {
+                    Username = newlyCreatedAccountUsername,
+                    IsAccountNewlyCreated = true
+                }, cancellationToken);
+                */
             }
-        }
 
-        foreach (var newlyCreatedAccountUsername in newlyCreatedAccountUsernames)
+            await _repository.SaveChangesAsync();
+
+            return Results.NoContent();
+        }
+        catch (Exception ex)
         {
-            /*
-            await _mediator.Send(new RequestPasswordReset
-            {
-                Username = newlyCreatedAccountUsername,
-                IsAccountNewlyCreated = true
-            }, cancellationToken);
-            */
+            return Results.BadRequest($"{ex.Message} {ex.StackTrace}");
         }
-
-        await _repository.SaveChangesAsync();
-
-        return Results.NoContent();
     }
 }

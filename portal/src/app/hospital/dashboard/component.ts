5be6a3dda7ace234c70cc8262cd6258da5a2a470
Nathan <nathand3v@gmail.com>
2026-08-06 17:48:25 +0100

    new dashboard

--- portal/src/app/hospital/dashboard/component.ts
+++ portal/src/app/hospital/dashboard/component.ts
@@ -1,44 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { getDashboard } from '../actions';
-import { Observable } from 'rxjs';
-import { Dashboard, ReadOnlyWrapper } from '../state';
-import { selectDashboard } from '../selectors';
-import { SpinnerComponent } from '../../shared/spinner/component';
-import { AsyncPipe } from '@angular/common';
-import { HospitalDashboardChartAdmissionsComponent } from './daily-admissions/component';
-import { HospitalDashboardAdmissionsByReasonComponent } from './admissions-by-reason/component';
-import { HospitalDashboardAdmissionsBySpeciesSurvivalComponent } from './admissions-by-species-survival/component';
-import { HospitalDashboardAdmissionsByDispositionComponent } from './admissions-by-disposition/component';
-import { HospitalDashboardAdmissionsBySpeciesRescuesComponent } from './admissions-by-species-rescue/component';
-import { HospitalDashboardAdmissionsBySpeciesEarlyLateMortalityComponent } from './admissions-by-species-early-late-mortality/component';
-import { HospitalDashboardChartInCareComponent } from './daily-in-care/component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'hospital-dashboard',
   standalone: true,
   templateUrl: './component.html',
   styleUrls: ['./component.scss'],
-  imports: [
-    AsyncPipe,
-    SpinnerComponent,
-    HospitalDashboardChartAdmissionsComponent,
-    HospitalDashboardChartInCareComponent,
-    HospitalDashboardAdmissionsByDispositionComponent,
-    HospitalDashboardAdmissionsByReasonComponent,
-    HospitalDashboardAdmissionsBySpeciesRescuesComponent,
-    HospitalDashboardAdmissionsBySpeciesSurvivalComponent,
-    HospitalDashboardAdmissionsBySpeciesEarlyLateMortalityComponent,
-  ],
 })
-export class HospitalDashboardComponent implements OnInit {
-  dashboard$: Observable<ReadOnlyWrapper<Dashboard>>;
-
-  constructor(private store: Store) {
-    this.dashboard$ = this.store.select(selectDashboard);
-  }
-
-  ngOnInit() {
-    this.store.dispatch(getDashboard());
-  }
-}
+export class HospitalDashboardComponent {}
